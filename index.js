9975954bf984364959dcc66e481808a5af4be5b1
Tushar <tusharmittal429@gmail.com>
2021-05-26 23:13:35 +0530

    updated new baseURL

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-// const baseURL = "https://file-share-app1.herokuapp.com";
-const baseURL = "http://localhost:3000";
+const baseURL = "https://file-sharing-final.herokuapp.com";
+// const baseURL = "http://localhost:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
